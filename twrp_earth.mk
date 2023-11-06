0e5c90ee017fb27866fdf212cd02fd821f95c12a
Anangtofa <149308422+Anangtofa@users.noreply.github.com>
2023-11-06 17:13:25 +0700

    Update twrp_earth.mk

--- twrp_earth.mk
+++ twrp_earth.mk
@@ -1,1 +1,45 @@
+#
+# Copyright (C) 2023 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit from earth device
+$(call inherit-product, device/xiaomi/earth/device.mk)
+
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := earth
+PRODUCT_NAME := twrp_earth
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Redmi 12c
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_earth-user 12 SP1A.210812.016 V13.0.3.0.SCVIDXM release-keys"
+
+BUILD_FINGERPRINT := Android/vnd_earth/earth:12/SP1A.210812.016/V13.0.3.0.SCVIDXM:user/release-keys
